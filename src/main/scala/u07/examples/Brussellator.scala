9ffecc0e128a8eca31505f317b794a899db3da07
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-10-26 18:27:29 +0200

    chore(guru): minor refactor

--- src/main/scala/u07/examples/Brussellator.scala
+++ src/main/scala/u07/examples/Brussellator.scala
@@ -5,6 +5,7 @@ import de.sciss.chart.api._
 import org.jfree.data.xy.XYSeriesCollection
 import org.jfree.data.xy.XYSeries
 import javax.sound.midi.Track
+import u07.modelling.CTMCSimulation.traceOfLength
 
 object Brussellator extends App:
   import u07.modelling.{CTMC, SPN}
@@ -23,7 +24,7 @@ object Brussellator extends App:
     Trn(MSet(X, X, Y), m => 1.0 * (m(X) * m(Y)), MSet(X, X, X), MSet()),
     Trn(MSet(B, X), m => 1.0 * (m(B) * m(X)), MSet(Y, D), MSet()),
     Trn(MSet(X), m => 1.0 * m(X), MSet(E), MSet()),
-    Trn(MSet(), m => 1.0, MSet(A, B), MSet())
+    Trn(MSet(), m => 1.0, MSet(A, B), MSet(A, B))
   )
 
   extension (n: Int) infix def of(s: Place): Array[Place] = Array.fill(n)(s)
@@ -31,13 +32,12 @@ object Brussellator extends App:
     def from(points: Iterable[(Double, Double)]): XYSeries =
       points.foreach(p => s.add(p._1, p._2))
       s
+  given Random = new Random()
 
   val trace = toCTMC(spn)
-    .newSimulationTrace(
-      MSet(((100 of X) ++ (100 of Y) ++ (100 of A) ++ (300 of B))*),
-      new Random
-    )
-    .take(20000)
+    .traceOfLength(
+      MSet(((100 of X) ++ (100 of Y) ++ (100 of A) ++ (300 of B))*)
+    )(20000)
 
   val series = XYSeriesCollection()
   Seq("[X]" -> X, "[Y]" -> Y).foreach: (name, place) =>
